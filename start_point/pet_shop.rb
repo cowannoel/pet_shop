7dfa7958202b916aa871fbc23bbdf682b28c6783
cowannoel <cowannoel@gmail.com>
2020-01-19 18:41:42 +0000

    eleven passing tests

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -27,6 +27,27 @@ def increase_pets_sold(pet_shop, sold)
   pet_shop[:admin][:pets_sold] += sold
 end
 
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].count
+end
+
+
+def pets_by_breed(pet_shop, breed)
+  breeds_array = []
+  for pet in pet_shop[:pets]
+    if breed == pet[:breed]
+      breeds_array.push(0)
+    end
+  end
+  return breeds_array
+  return nil
+end
+
+
+
+
+
 def find_pet_by_name (pet_shop, pet_name)
   for pet in pet_shop[:pets]
     if pet_name == pet[:name]
@@ -35,6 +56,7 @@ def find_pet_by_name (pet_shop, pet_name)
   end
   return nil
 end
+
 
 
 
