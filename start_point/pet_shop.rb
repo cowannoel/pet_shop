900af31873a1f482e6c7afb97cce69b007576b35
cowannoel <cowannoel@gmail.com>
2020-01-19 00:16:41 +0000

    midway point

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -26,3 +26,43 @@ end
 def increase_pets_sold(pet_shop, sold)
   pet_shop[:admin][:pets_sold] += sold
 end
+
+def find_pet_by_name (pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet_name == pet[:name]
+      return pet
+    end
+  end
+end
+
+# def find_pet_by_name (pet_shop, pet_name)
+#   for pet in pet_shop[:pets]
+#     if pet_name == pet[:name]
+#       return nil
+#     end
+#   end
+# end
+
+
+def remove_pet_by_name(pet_shop, remove_pet_by_name)
+  for pet in pet_shop[:pets]
+    if remove_pet_by_name == pet[:name].upshift
+  end
+end
+end
+
+
+
+
+
+
+
+# def customer_pet_count(customers_array)
+#   total_customer_pets = []
+#   for customer in customers_array[:pets]
+#     total_customer_pets += customer
+#   end
+#   return total_customer_pets.length()
+# end
+#
+#
